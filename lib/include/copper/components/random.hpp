2c464e5ca1269c7ae2eb529a0dc2fd2028f45a09
Ian Torres <iantorres@outlook.com>
2025-02-08 17:02:33 -0300

    Docblock added.

--- lib/include/copper/components/random.hpp
+++ lib/include/copper/components/random.hpp
@@ -4,6 +4,12 @@
 
 namespace copper::components::random {
 
+    /**
+     * Generate a random string
+     *
+     * @param size
+     * @return
+     */
     std::string string(int size = 16);
 
 }  // namespace copper::components::strong
