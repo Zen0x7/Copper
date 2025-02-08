bf68096b15071345a0c5d28700ad7a937db1dc75
Ian Torres <iantorres@outlook.com>
2025-02-08 11:23:48 -0300

    Base64 added.

---
+++ tests/components/base64url_test.cc
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+
+#include <copper/components/base64url.hpp>
+
+using namespace copper::components;
+
+TEST(Components_Base64url, Encode) {
+    const std::string input = "hello+world//";
+
+    const std::string output = base64url::encode(input);
+    ASSERT_EQ(output, "aGVsbG8rd29ybGQvLw==");
+
+    const std::string output_no_padding = base64url::encode(input, false);
+    ASSERT_EQ(output_no_padding, "aGVsbG8rd29ybGQvLw");
+}
+
+TEST(Components_Base64url, Decode) {
+    const std::string input = "aGVsbG8rd29ybGQvLw==";
+
+    const std::string output = base64url::decode(input);
+    ASSERT_EQ(output, "hello+world//");
+
+    const std::string input_no_padding = "aGVsbG8rd29ybGQvLw";
+    const std::string output_no_padding = base64url::decode(input_no_padding);
+    ASSERT_EQ(output_no_padding, "hello+world//");
+}
+
