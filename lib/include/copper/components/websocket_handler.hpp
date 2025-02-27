bbf9ccc7c6c1ec5033b7d3261ef35ed6af851600
Ian Torres <iantorres@outlook.com>
2025-02-27 13:11:39 -0300

    Rename vars.

--- lib/include/copper/components/websocket_handler.hpp
+++ lib/include/copper/components/websocket_handler.hpp
@@ -30,24 +30,24 @@ namespace copper::components {
  */
 template <typename Stream>
 containers::async_of<void> websocket_handler(
-    shared<state> & /* state */, uuid /* server_id */, uuid /* session_id */,
+    shared<state> & /*state*/, uuid /* server_id */, uuid /* session_id */,
     Stream &stream, boost::beast::flat_buffer &buffer, request req,
     boost::beast::string_view) {
-  auto _cs = co_await boost::asio::this_coro::cancellation_state;
-  auto _ws = boost::beast::websocket::stream<Stream &>{stream};
+  auto _cancellation_state = co_await boost::asio::this_coro::cancellation_state;
+  auto _connection = boost::beast::websocket::stream<Stream &>{stream};
 
-  _ws.set_option(boost::beast::websocket::stream_base::timeout::suggested(
+  _connection.set_option(boost::beast::websocket::stream_base::timeout::suggested(
       boost::beast::role_type::server));
 
-  _ws.set_option(boost::beast::websocket::stream_base::decorator(
+  _connection.set_option(boost::beast::websocket::stream_base::decorator(
       [](boost::beast::websocket::response_type &response) {
         response.set(boost::beast::http::field::server, "Copper");
       }));
 
-  co_await _ws.async_accept(req);
+  co_await _connection.async_accept(req);
 
-  while (!_cs.cancelled()) {
-    auto [_ec, _] = co_await _ws.async_read(buffer, boost::asio::as_tuple);
+  while (!_cancellation_state.cancelled()) {
+    auto [_ec, _] = co_await _connection.async_read(buffer, boost::asio::as_tuple);
 
     if (_ec == boost::beast::websocket::error::closed ||
         _ec == boost::asio::ssl::error::stream_truncated)
@@ -55,13 +55,13 @@ containers::async_of<void> websocket_handler(
 
     if (_ec) throw boost::system::system_error{_ec};
 
-    _ws.text(_ws.got_text());
-    co_await _ws.async_write(buffer.data());
+    _connection.text(_connection.got_text());
+    co_await _connection.async_write(buffer.data());
 
     buffer.consume(buffer.size());
   }
 
-  auto [_ec] = co_await _ws.async_close(
+  auto [_ec] = co_await _connection.async_close(
       boost::beast::websocket::close_code::service_restart,
       boost::asio::as_tuple);
 
