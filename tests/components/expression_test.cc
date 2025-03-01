3aa7de5b7124e6d8c1bee490bd6bac62adf93074
Ian Torres <iantorres@outlook.com>
2025-02-28 22:08:53 -0300

    Multi expression supported.

--- tests/components/expression_test.cc
+++ tests/components/expression_test.cc
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <copper/components/expression.hpp>
+#include <iostream>
 
 TEST(Components_Expression, Assertions) {
   using namespace copper::components;
@@ -25,4 +26,20 @@ TEST(Components_Expression, Assertions) {
   ASSERT_TRUE(_integer_result->matches());
   ASSERT_FALSE(_integer_result->get_bindings().empty());
   ASSERT_EQ(_integer_result->get("x"), "7");
+
+  const auto _multi_expression = expression_make("/u/{x}/d/{y}/x/{z}");
+
+  ASSERT_FALSE(_multi_expression->get_arguments().empty());
+  ASSERT_EQ(_multi_expression->get_arguments().size(), 3);
+  ASSERT_EQ(_multi_expression->get_arguments().at(0), "x");
+  ASSERT_EQ(_multi_expression->get_arguments().at(1), "y");
+  ASSERT_EQ(_multi_expression->get_arguments().at(2), "z");
+
+  const auto _multi_expression_result = _multi_expression->query("/u/c80b/d/5/x/7");
+
+  ASSERT_TRUE(_multi_expression_result->matches());
+  ASSERT_FALSE(_multi_expression_result->get_bindings().empty());
+  ASSERT_EQ(_multi_expression_result->get("x"), "c80b");
+  ASSERT_EQ(_multi_expression_result->get("y"), "5");
+  ASSERT_EQ(_multi_expression_result->get("z"), "7");
 }
